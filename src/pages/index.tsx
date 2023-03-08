be302e6c3f37b2ac05009b5b3a8ea7106eb36300
kouhai-san <kouhaisan.yorimichi@gmail.com>
2023-03-09 02:58:49 +0900

    feat: :tada: Create New Projext

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Lesson1 from "@/components/Lesson1"
+
+export default function HomePage() {
+  return (
+    <>
+        <div>
+            <main>
+                <Lesson1 />            
+            </main>
+        </div>    
+    </>
+
+  )
+}
