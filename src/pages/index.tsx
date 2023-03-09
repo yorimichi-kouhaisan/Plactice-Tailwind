a07021775edc8b7708cdb76048ef99608376f483
kouhai-san <kouhaisan.yorimichi@gmail.com>
2023-03-09 22:49:17 +0900

    feat: :sparkles: Lesson2 is done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Lesson1 from "@/components/Lesson1"
+import Lesson2 from "@/components/Lesson2"
 
 export default function HomePage() {
   return (
     <>
         <div>
             <main>
-                <Lesson1 />            
+                <Lesson2 />            
             </main>
         </div>    
     </>
